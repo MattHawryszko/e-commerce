3a02f552c3ab0ce316a5ecfd31eb92f4c8ee5932
MattHawryszko <46821330+MattHawryszko@users.noreply.github.com>
2019-04-28 14:12:42 -0400

    Api images upload and view testing

--- src/scenes/shop.js
+++ src/scenes/shop.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-
+const axios = require('axios')
 export default class App extends Component {
+
+
+uploadImage = event => { 
+  const files = document.getElementById('INPUT_TAG').files
+  const formData = new FormData()
+  formData.append('image', files[0])
+  axios.post('http://127.0.0.1:8080/images', formData)
+  console.log('testing')
+}
   componentDidMount(){
     
   }
@@ -13,6 +22,9 @@ export default class App extends Component {
         <Navbar history={this.props.history} />
       	Shop
         <div className="p-5"></div>
+        <input type="file" id="INPUT_TAG"/>
+        <button onClick={this.uploadImage}>call uploadImage()</button>
+        <img src="http://127.0.0.1:8080/images/5cc5ea9aafb5bf370a1d7cad"></img>
         <Footer history={this.props.history}/>
       </div>
     );
